d8e5b6b2b6fbf3f6ad7d08b94428515faca932fb
Lucas Sobral <lucas.sobral@jfpe.jus.br>
2020-08-18 16:36:37 -0300

    Edição de formulario

---
+++ API/EstudoFrontEnd/Controllers/UsuarioController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using EstudoFrontEnd.Classes;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace EstudoFrontEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {        
+
+        [HttpGet()]
+        public ActionResult<IEnumerable<int>> Get()
+        {
+            Random rnd = new Random();
+            
+            return Ok(JsonConvert.SerializeObject(rnd.Next(1, 1000)));
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
